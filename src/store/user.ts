47d5e44c4d3c86aeeb54ab36a12b10092a36f459
Joe.Chen <wwwkkpop0218@gmail.com>
2024-04-10 17:34:00 +0800

    feat: 新增權限常量、判斷權限函數

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,12 +4,35 @@ export const useUserStore = defineStore('user', {
     // 存放共享資料的地方
     state() {
         return {
-            id: '',     // 使用者識別碼
-            name: '',   // 使用者名稱
-            roles: '',   // 角色權限
+            id: '',         // 使用者識別碼
+            name: '',       // 使用者名稱
+            roles: '',      // 使用者角色權限
+            roleType: {     // 角色權限常量
+                admin: 'ADMIN',
+                teacher: 'TEACHER',
+                student: 'STUDENT',
+            },
         }
     },
-    actions: {},
+    actions: {
+
+        /**
+         * 判斷是否有權限
+         * @param roleArr 權限陣列
+         * @return true有權限, false無權限
+         */
+        isPass(roleArr: Array<any>) {
+            let selfArr = this.roles.split("-");
+            for (let role of roleArr) {
+                for (let string of selfArr) {
+                    if (string == role) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+    },
     getters: {},
     // 持久化
     persist: {
