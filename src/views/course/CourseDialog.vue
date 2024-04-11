dce62a4247ed6c1ad656d484e00af3c8ae108d05
Joe.Chen <wwwkkpop0218@gmail.com>
2024-04-11 14:08:21 +0800

    feat: 添加課程新增的驗證

--- src/views/course/CourseDialog.vue
+++ src/views/course/CourseDialog.vue
@@ -11,6 +11,16 @@
           <el-input v-model="form.name" placeholder="請輸入課程名稱"/>
         </el-form-item>
 
+        <div v-if="showTeacherOption">
+          <el-form-item prop="teacher" label="老師">
+            <el-select v-model="form.teacher" placeholder="請選擇老師">
+              <div v-for="item in teachersData" :key="item.id">
+                <el-option :label="item.name" :value="item.id"/>
+              </div>
+            </el-select>
+          </el-form-item>
+        </div>
+
         <el-form-item prop="maxCount" label="課程人數" style="width: 180px">
           <el-input v-model.number="form.maxCount" placeholder="課程人數"/>
         </el-form-item>
@@ -95,24 +105,29 @@
 </template>
 
 <script setup lang="ts">
-import {computed, inject, onBeforeMount, reactive, ref} from 'vue'
+import {computed, inject, nextTick, onBeforeMount, reactive, ref, watch, watchEffect} from 'vue'
 import {ElMessage, ElMessageBox, type FormInstance} from 'element-plus'
 import http from "@/util/request";
 import {useUserStore} from "@/store/user"
 
-const userStore = useUserStore()
+const userStore: any = useUserStore()
 
 // 掛載前執行
 onBeforeMount(() => {
-
+  // 身分是老師時，自動給予老師ID
+  if (userStore.identity == userStore.identityType.teacher) {
+    form.teacher = userStore.id
+  }
 })
 
 
 // 刷新頁面用
 const reload: any = inject("reload");
-const BASE_URL: any = inject('COURSE_BASE_URL')
 
+// 地址
+const BASE_URL: any = inject('COURSE_BASE_URL')
 const LOCATION_URL = '/courseLocation'
+const TEACHER_URL = '/teacher'
 
 // 控制彈出框顯示
 const dialogVisible = ref(false)
@@ -121,11 +136,12 @@ const dialogVisible = ref(false)
 let dialogTitle = ref('')
 let dialogConfirm = ref('')
 
-// 新增框 彈出
+// 新增框彈出
 const addDialogPop = (() => {
   dialogTitle.value = '新增'
   dialogConfirm.value = '新增'
   getLocation()
+  getTeachers()
   dialogVisible.value = true
 })
 // 修改框 彈出
@@ -157,13 +173,22 @@ let showLocationText = computed(() => {
   return form.location == 'OTHER'
 })
 
+// 顯示「老師下拉框」判斷的屬性
+let showTeacherOption = computed(() => {
+  const roleType = userStore.roleType
+  let arr = [
+    roleType.admin
+  ]
+  return userStore.isPass(arr)
+})
+
 // 表單物件
 const formRef = ref()
 
 // 表單資料
 let form = reactive({
   name: '',
-  teacher: userStore.id,
+  teacher: '',
   maxCount: '',
   deadline: '',
   startDate: '',
@@ -174,19 +199,67 @@ let form = reactive({
   otherLocation: '',
 })
 
+// 清除驗證錯誤提示監視
+watchEffect(() => {
+  // 清除「老師」欄位驗證提示 (內容為空 且 是管理員身分)
+  if (form.teacher != '' && userStore.identity == userStore.identityType.admin){
+    nextTick(formRef.value.clearValidate('teacher'))
+  }
+
+  // 清除「地點」欄位驗證提示
+  if (form.location != ''){
+    nextTick(formRef.value.clearValidate('location'))
+  }
+
+  // 清除「上課時間」欄位驗證提示
+  if (form.startTime != '' && form.endTime != '') {
+    console.log('都有選了')
+    nextTick(formRef.value.clearValidate(['courseTime']))
+  }
+})
+
+// 驗證課程日期
+const validateCourseDate = (rule: any, value: any, callback: any) => {
+  if (form.startDate == '' && form.endDate == '') {
+    callback(new Error('請選擇課程日期'))
+  } else if (form.startDate == '') {
+    callback(new Error('請選擇課程開始日期'))
+  } else if (form.endDate == '') {
+    callback(new Error('請選擇課程結束日期'))
+  } else {
+    nextTick(formRef.value.clearValidate(['courseDate']))
+    callback()
+  }
+}
+// 驗證上課時間
+const validateCourseTime = (rule: any, value: any, callback: any) => {
+  if (form.startTime == '' && form.endTime == '') {
+    callback(new Error('請選擇上課時間'))
+  } else if (form.startTime == '') {
+    callback(new Error('請選擇上課開始時間'))
+  } else if (form.endTime == '') {
+    callback(new Error('請選擇上課結束時間'))
+  } else {
+    callback()
+  }
+}
+
 // 表單驗證規則
 const courseRules = reactive({
-  name: [{required: true, message: '請輸入課程名稱', trigger: 'blur'},],
-  maxCount: [{required: true, message: '請輸入課程人數', trigger: 'blur'},],
-  deadline: [{required: true, message: '請選擇報名截止日期', trigger: 'blur'},],
-  courseDate: [{required: true, message: '請選擇課程日期', trigger: 'blur'},],
-  courseTime: [{required: true, message: '請選擇上課時間', trigger: 'blur'},],
-  location: [{required: true, message: '請選擇上課地點', trigger: 'blur'},],
+  name: [{required: true, message: '請輸入課程名稱', trigger: 'blur'}],
+  teacher: [{required: true, message: '請選擇老師', trigger: 'blur'}],
+  maxCount: [{required: true, message: '請輸入課程人數', trigger: 'blur'}],
+  deadline: [{required: true, message: '請選擇報名截止日期', trigger: 'blur'}],
+  courseDate: [{validator: validateCourseDate, trigger: 'blur'}],
+  courseTime: [{validator: validateCourseTime, trigger: 'blur'}],
+  location: [{required: true, message: '請選擇上課地點', trigger: 'blur'}],
 
 })
 
-// 頁面資料
+// 「地點」下拉選框資料
 let locationData: any = ref([])
+// 「老師」下拉選框資料
+let teachersData: any = ref([])
 
 // 設置結束日期
 const disabledDateEnd = (time: Date) => {
@@ -252,11 +325,20 @@ async function addCourse() {
 // 獲取地點函數
 async function getLocation() {
   const {data: res} = await http.get(LOCATION_URL)
-  console.log(res)
   if ('200' == res.code) {
     locationData.value = res.data
   }
 }
+
+// 獲取老師函數
+async function getTeachers() {
+  const {data: res} = await http.get(TEACHER_URL)
+  if ('200' == res.code) {
+    teachersData.value = res.data
+  }
+}
+
+
 
 // 對外曝露組件內容
 defineExpose({addDialogPop})
